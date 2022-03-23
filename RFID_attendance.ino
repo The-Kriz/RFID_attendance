707ad517854d52ecaf01f9150f7103d39b097c50
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-03-23 23:28:36 +0530

    bug fix

--- RFID_attendance.ino
+++ RFID_attendance.ino
@@ -3,38 +3,30 @@
 #include <MFRC522.h>
 
 //pins & attribute values
-#define RELAY 8
 #define SS_PIN 10
 #define RST_PIN 9
-#define ACCESS_DELAY 5000 // time the door should be opened for
 #define DELAY 5000
-#define members 3 // enter number of people who has access
+#define Students 3 // enter number of people who has access
 
 // User Tag Id and User name 
 const char* verified_rfid[] = 
 {
 "70 82 7A 89",
 "1B 6F B3 73",
-"14 8F CE 33",
-"54 43 C3 33"
 } ;// Enter UID of the cards that need the access to the room
 const char* employ[]=
 {
 "Akashy antony",
 "Nived k",
-"Harikrishnan M",
-"Aryan Athul"
 };
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() 
 {
-  pinMode(RELAY, OUTPUT);
   Serial.begin(9600);  
   SPI.begin();          
   mfrc522.PCD_Init();  
-  digitalWrite(RELAY, LOW);
 
   Serial.println("Put your card to the reader...");
   Serial.println();
@@ -67,25 +59,20 @@ void loop()
   Serial.print("Message : ");
   content.toUpperCase();
   
-  for(int j = 0;j < members ;j++)
+  for(int j = 0;j < Students ;j++)
     {
     if (content.substring(1) == verified_rfid[j])
       {
-        Serial.println("Authorized access");
         Serial.println(employ[j]);
+        Serial.println("Attendance Marked");
         foundTag = true;
         Serial.println();
-        digitalWrite(RELAY, HIGH);
-        delay(ACCESS_DELAY);
-        digitalWrite(RELAY, LOW);
         delay(DELAY);
-      }}
+      }
     if (!foundTag)
         {
-          Serial.println(" Access denied");
-          Serial.println("Unauthorized User");
-          delay(DELAY)
-         ;
+          Serial.println("Student Details not found!");
+          delay(DELAY);
         }
       
-}
+    }}
